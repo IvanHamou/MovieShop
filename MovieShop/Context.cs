4fb3d96ff79e1d96be6d702ef498bcf173442cbc
Ivan Hamou <ivanhamou@gmail.com>
2020-12-02 13:41:39 +0100

    Created Context.cs, added DbContext, OnConfiguring and OnModelCreating

---
+++ MovieShop/Context.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DatabaseConnection
+{
+    public class Context : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLazyLoadingProxies()
+                .UseSqlServer(
+                @"server=.\SQLExpress;" +
+                @"database=SaleDatabase;" +
+                @"trusted_connection=true;" +
+                @"MultipleActiveResultSets=True"
+                );
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
