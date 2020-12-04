02e755efd21334bb73aca2087e8694a2ec3efa9c
Ivan Hamou <ivanhamou@gmail.com>
2020-12-04 15:16:15 +0100

    Created DatabaseAPI.cs and added migration for movies

---
+++ MovieShop/DatabaseAPI.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseConnection
+{
+    public static class DatabaseAPI
+    {
+
+        static Context ctx;
+
+        static DatabaseAPI()
+        {
+            ctx = new Context();
+        }
+
+        public static List<Movie> GetMovieSlice(int skip_x, int take_x)
+        {
+            return ctx.Movies
+                .OrderBy(m => m.Title)
+                .Skip(skip_x)
+                .Take(take_x)
+                .ToList();
+        }
+        public static Customer GetCustomerByName(string name)
+        {
+            return ctx.Customers
+                .FirstOrDefault(c => c.Name.ToLower() == name.ToLower());
+        }
+        public static bool RegisterSale(Customer customer, Movie movie)
+        {
+            try
+            {
+                ctx.Add(new Sale() { Date = DateTime.Now, Customer = customer, Movie = movie });
+
+                bool one_record_added = ctx.SaveChanges() == 1;
+                return one_record_added;
+            }
+            catch (DbUpdateException e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+                System.Diagnostics.Debug.WriteLine(e.InnerException.Message);
+                return false;
+            }
+        }
+    }
+}
+
+    
